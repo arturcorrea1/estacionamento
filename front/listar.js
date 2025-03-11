c9b31e9145ce74f041c2ed067dbef4400b7b32ca
Artur Corrêa <arturcorrea8667@gmail.com>
2025-03-10 21:54:33 -0300

    modal de editar

--- front/listar.js
+++ front/listar.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <td>${carro.cpf}</td>
             <td>${carro.vaga}</td>
             <td class="delete-btn" onclick="excluir('${carro.placa}')">✖</td>
-            <td class="editar-btn" onclick="abrirModal('${carro.dono}', '${carro.tipo}', '${carro.cpf}', '${carro.vaga}', '${carro.placa}')">Editar</td>
+            <td class="editar-btn" onclick="abrirModal('${carro.dono}', '${carro.placa}', '${carro.cpf}', '${carro.tipo}', '${carro.vaga}')">Editar</td>
         `;
         tbody.appendChild(row);
     });
@@ -44,7 +44,7 @@ async function excluir(placa) {
 }
 
 // Função para abrir o modal e preencher os campos
-function abrirModal(dono, placa, cpf, vaga, placaAntigaParam) {
+function abrirModal(dono, placa, cpf, tipo, vaga, placaAntigaParam) {
     placaAntiga = placaAntigaParam; 
     document.getElementById('dono').value = dono;
     document.getElementById('placa').value = placa;
@@ -53,7 +53,7 @@ function abrirModal(dono, placa, cpf, vaga, placaAntigaParam) {
     document.getElementById('vaga').value = vaga;
 
     // Exibir o modal
-    document.getElementById('modal').style.display = 'block';
+    document.getElementById('modal').style.display = 'flex';
     
 }
 
@@ -63,7 +63,7 @@ function fecharModal() {
 }
 
 async function editar() {
-    const dono = document.getElementById('nome').value;
+    const dono = document.getElementById('dono').value;
     const placaNova = document.getElementById('placa').value;
     const cpf = document.getElementById('cpf').value;
     const tipo = document.getElementById('tipo').value;
@@ -71,19 +71,19 @@ async function editar() {
 
     console.log('Dados enviados:', { dono, placa, cpf, tipo, vaga });
 
-    const response = await fetch(`http://localhost:3003/editar/${placaAntiga}`, {
+    const response = await fetch(`http://localhost:3000/editar/${placaAntiga}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({placaAntiga, dono, placaNova, cpf, vaga })
+        body: JSON.stringify({placaAntiga, dono, placaNova, cpf, tipo, vaga })
     });
 
     const result = await response.json();
     if (result.success) {
         alert('Carro editado com sucesso!');
-        listar(); // Recarrega a lista após a edição
-        fechar(); // Fecha o modal
+        listar();
+        fechar(); 
     } else {
         alert(result.message || 'Erro ao editar o carro!');
     }
